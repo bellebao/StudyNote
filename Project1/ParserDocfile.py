c421e8605ce2b621312a48da9074c03a5eca3bd3
belle <belle.bao@gmail.com>
2018-03-19 16:31:57 +0800

    update

--- Project1/ParserDocfile.py
+++ Project1/ParserDocfile.py
@@ -5,11 +5,14 @@ Spyder Editor
 This is a temporary script file.
 """
 import win32com.client
-
+import os
+import string
 #--------------------------------CONSTANTS-------------------------------------
 wdFormatText = 2
 
+#--------------------------------GLOBALS---------------------------------------
 
+filenum = 0
 #--------------------------------FUNCTIONS-------------------------------------
 def word2txt(docfilename):
     app = win32com.client.Dispatch('Word.Application')
@@ -19,10 +22,37 @@ def word2txt(docfilename):
     doc.Close()
     return txtfilename
 
+
+def ParseFile(path):
+    global filenum    
+    if os.path.isdir(path):
+        filenum = len(os.listdir(path))
+        for sub_dir in os.listdir(path):
+            filename = os.path.join(path, sub_dir)
+            ParseFile(filename)
+    else:
+        TraceParser(path)
+        
+def TraceParser(filename):
+    global filenum
+    filename = str.lower(filename)
+    index = filename.rfind('.')
+    
+    if (index != -1):
+        fileext = filename[index:]
+    
+    if (fileext in [".doc", ".c", ".cpp", ".s", ".h", ".hpp"]):
+        
+        filenum = filenum + 1    
+        if filename.endswith(".doc"):
+            fullname = word2txt(filename)
+        else:
+            fullname = filename  
+
 def cli():
     
-    filename = "C:\IOSystems\Docs\E435157_2017_Goals.doc"
-    word2txt(filename)
+    filename = "C:\IOSystems\\Temp"
+    ParseFile(filename)
 
 if __name__ == "__main__":
     cli()
